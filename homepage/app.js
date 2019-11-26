87325fc551cdd7f9270ebd5c2a9cac69f7576947
songw3n <songw3n@outlook.com>
2019-11-26 14:41:24 -0500

    re-structured

--- homepage/app.js
+++ homepage/app.js
@@ -25,7 +25,7 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
-app.use("/playground", express.static("playground/dist"));
+app.use("/playground", express.static("../playground/dist"));
 
 
 app.use(require("express-session")({
